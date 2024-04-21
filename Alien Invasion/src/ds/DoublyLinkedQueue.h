7563484938a9b50e6fbfcbf89094ceb09c065740
Salah_Mostafa <149152392+Salah3060@users.noreply.github.com>
2024-04-21 10:26:00 +0200

    New Version

--- Alien Invasion/src/ds/DoublyLinkedQueue.h
+++ Alien Invasion/src/ds/DoublyLinkedQueue.h
@@ -4,28 +4,27 @@ template<class T>
 class DoublyLinkedQueue :public LinkedQueue<T>
 {
 public:
-	bool DequeueFromBack(Node<T>* Back)
-	{
-		if (!backPtr)
-			return false;
-
-		if (backPtr == frontPtr){
-			T Data = 0;
-			dequeue(Back, Data);
-		}
-
-		else {
-			Back = backPtr;
-			backPtr = backPtr->getPrev();
-			backPtr->setNext(nullptr);
-		}
-		return true;
-	}
-	bool DoublyDequeue(Node<T>* Front, Node<T>* Back)
-	{
-		if (dequeue(Front) && DequeueFromBack(Back))
-			return true;
-		return false;
-	}
+	//	bool DequeueFromBack(Node<T>* Back)
+	//	{
+	//		if (!backPtr)
+	//			return false;
+	//
+	//		if (backPtr == frontPtr){
+	//			T Data = 0;
+	//			dequeue(Back, Data);
+	//		}
+	//
+	//		else {
+	//			Back = backPtr;
+	//			backPtr = backPtr->getPrev();
+	//			backPtr->setNext(nullptr);
+	//		}
+	//		return true;
+	//	}
+	//	bool DoublyDequeue(Node<T>* Front, Node<T>* Back)
+	//	{
+	//		if (dequeue(Front) && DequeueFromBack(Back))
+	//			return true;
+	//		return false;
+	//	}
 };
-
